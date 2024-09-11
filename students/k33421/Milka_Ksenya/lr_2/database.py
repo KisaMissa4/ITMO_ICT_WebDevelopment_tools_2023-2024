aa2ab0b6b79334c022724f5385b8611b355d0196
Ksenya <00000087@mail.ru>
2024-09-11 15:26:13 +0300

    First implementation

---
+++ students/k33421/Milka_Ksenya/lr_2/database.py
@@ -0,0 +1,21 @@
+import asyncpg
+
+
+def get_connection():
+    return asyncpg.connect(
+        host='localhost',
+        port=1212,
+        database="database",
+        user="postgres",
+        password="postgres",
+    )
+
+
+def insert_data_to_project(data):
+    connection = get_connection()
+
+    await connection.execute(
+        """
+        INSERT INTO project (name, description, deadline)
+        """
+    )
